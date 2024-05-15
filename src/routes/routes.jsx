1de5753627817335dad57ed4c2b24085f8334529
Mariya Belinskaya <mv.belinskaya@gmail.com>
2024-05-15 19:33:35 +0500

    added information pages

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import HomePage from "../containers/HomePage/HomePage.jsx";
 import CharacterPage from "../containers/filmPage/CharacterPage.jsx";
+import PersonPage from "../containers/PersonPage/PersonPage.jsx";
 
 const routes = [
   { path: "/", element: <HomePage /> },
   { path: "/people", element: <CharacterPage /> },
+  { path: "/people/:id", element: <PersonPage /> },
 ];
 
 export default routes;
